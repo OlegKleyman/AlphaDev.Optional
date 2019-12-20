125260a6215d8ba01864514d393e35a8d8c23e4c
Omego2K <omego2k@gmail.com>
2019-12-19 22:37:57 -0500

    import existing project

---
+++ tests/unit/AlphaDev.Optional.Extensions.Tests.Unit/TaskExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Optional;
+using Xunit;
+
+namespace AlphaDev.Optional.Extensions.Tests.Unit
+{
+    public class TaskExtensionsTests
+    {
+        [Fact]
+        public async Task SomeNotEmptyAsyncReturnsNoneWhenEnumerableIsEmpty()
+        {
+            var exception = new object();
+            var result = await Task.FromResult(Enumerable.Empty<int>()).SomeNotEmptyAsync(() => exception);
+            result.Should().Be(Option.None<IEnumerable<int>, object>(exception));
+        }
+
+        [Fact]
+        public async Task SomeNotEmptyAsyncReturnsSomeWhenEnumerableIsNotEmpty()
+        {
+            var enumerable = new[] { 1 }.AsEnumerable();
+            var result = await Task.FromResult(enumerable).SomeNotEmptyAsync(() => new object());
+            // ReSharper disable once PossibleMultipleEnumeration - enumerable is an array
+            result.Should().Be(enumerable.Some().WithException(new object()));
+        }
+    }
+}
---
+++ tests/unit/AlphaDev.Optional.Extensions.Tests.Unit/ValueTaskExtensionsTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Optional;
+using Xunit;
+
+namespace AlphaDev.Optional.Extensions.Tests.Unit
+{
+    public class ValueTaskExtensionsTests
+    {
+        [Fact]
+        public static async Task SomeNotNullAsyncValueTaskReturnsExceptionNoneWhenTargetNull()
+        {
+            var option =
+                await new ValueTask<string?>(Task.FromResult(default(string?))).SomeNotNullAsync(() => "exception");
+            option.Should().Be(Option.None<string, string>("exception"));
+        }
+
+        [Fact]
+        public static async Task SomeNotNullAsyncValueTaskReturnsNoneWhenTargetNull()
+        {
+            var option = await new ValueTask<object?>(Task.FromResult(default(object?))).SomeNotNullAsync();
+            option.Should().Be(Option.None<object>());
+        }
+
+        [Fact]
+        public static async Task SomeNotNullAsyncValueTaskReturnsSomeWhenTargetNotNull()
+        {
+            var option = await new ValueTask<string>(Task.FromResult("test")).SomeNotNullAsync();
+            option.Should().Be("test".Some());
+        }
+
+        [Fact]
+        public static async Task SomeNotNullAsyncValueTaskWithExceptionReturnsSomeWhenTargetNotNull()
+        {
+            var option = await new ValueTask<string>(Task.FromResult("test")).SomeNotNullAsync(() => new object());
+            option.Should().Be("test".Some<string, object>());
+        }
+    }
+}
