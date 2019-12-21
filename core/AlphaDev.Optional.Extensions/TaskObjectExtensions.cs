b8d66d2c7be7d43ca14faca70ae9d1c0a1292c48
Omego2K <omego2k@gmail.com>
2019-12-21 08:59:51 -0500

    add additional convenience methods

---
+++ core/AlphaDev.Optional.Extensions/TaskObjectExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Threading.Tasks;
+using Optional;
+
+namespace AlphaDev.Optional.Extensions
+{
+    public static class TaskObjectExtensions
+    {
+        public static async Task<Option<TEnumerable>> SomeNotEmptyAsync<TEnumerable>(
+            this Task<TEnumerable> task)
+            where TEnumerable : IEnumerable
+        {
+            return (await task).SomeWhen(enumerable => enumerable.GetEnumerator().MoveNext());
+        }
+
+        public static async Task<Option<TEnumerable, TException>> SomeNotEmptyAsync<TEnumerable, TException>(
+            this Task<TEnumerable> task, Func<TException> exceptionFactory)
+            where TEnumerable : IEnumerable
+        {
+            return (await task).SomeWhen(enumerable => enumerable.GetEnumerator().MoveNext(), exceptionFactory);
+        }
+
+        public static async Task<Option<T>> SomeNotNullAsync<T>(this Task<T> task) => (await task).SomeNotNull();
+
+        public static async Task<Option<T, TException>> SomeNotNullAsync<T, TException>(this Task<T> task,
+            Func<TException> exceptionFactory) => (await task).SomeNotNull(exceptionFactory);
+
+        public static async Task<Option<T>> SomeAsync<T>(this Task<T> task) => (await task).Some();
+
+        public static async Task<Option<T, TException>> SomeAsync<T, TException>(this Task<T> task) =>
+            (await task).Some<T, TException>();
+
+
+        public static Task<Option<T>> NoneAsync<T>(
+#pragma warning disable IDE0060 // Remove unused parameter - this is purposely unused for a convenience method
+            this Task<T> task
+#pragma warning restore IDE0060 // Remove unused parameter
+        ) => Task.FromResult(Option.None<T>());
+
+        public static async Task<Option<T, TException>> NoneAsync<T, TException>(this Task<T> task,
+            TException exception) => (await task).None(exception);
+    }
+}
--- tests/unit/AlphaDev.Optional.Extensions.Tests.Unit/TaskExtensionsTests.cs
+++ tests/unit/AlphaDev.Optional.Extensions.Tests.Unit/TaskExtensionsTests.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
-using Optional;
+using FluentAssertions.Optional.Extensions;
 using Xunit;
 
 namespace AlphaDev.Optional.Extensions.Tests.Unit
@@ -10,20 +9,94 @@ namespace AlphaDev.Optional.Extensions.Tests.Unit
     public class TaskExtensionsTests
     {
         [Fact]
-        public async Task SomeNotEmptyAsyncReturnsNoneWhenEnumerableIsEmpty()
+        public async Task NoneAsyncEitherReturnsNone()
+        {
+            var target = "test";
+            var result = await Task.FromResult(default(object)).NoneAsync(target);
+            result.Should().BeNone().Which.Should().BeSameAs(target);
+        }
+
+        [Fact]
+        public async Task NoneAsyncReturnsSome()
+        {
+            var result = await Task.FromResult(default(object)).NoneAsync();
+            result.Should().BeNone();
+        }
+
+        [Fact]
+        public async Task SomeAsyncEitherReturnsSome()
+        {
+            var target = new object();
+            var result = await Task.FromResult(target).SomeAsync<object, string>();
+            result.Should().HaveSome().Which.Should().BeSameAs(target);
+        }
+
+        [Fact]
+        public async Task SomeAsyncReturnsSome()
+        {
+            var target = new object();
+            var result = await Task.FromResult(target).SomeAsync();
+            result.Should().HaveSome().Which.Should().BeSameAs(target);
+        }
+
+        [Fact]
+        public async Task SomeNotEmptyAsyncEitherReturnsNoneWhenEnumerableIsEmpty()
         {
             var exception = new object();
             var result = await Task.FromResult(Enumerable.Empty<int>()).SomeNotEmptyAsync(() => exception);
-            result.Should().Be(Option.None<IEnumerable<int>, object>(exception));
+            result.Should().BeNone().Which.Should().BeSameAs(exception);
+        }
+
+        [Fact]
+        public async Task SomeNotEmptyAsyncEitherReturnsSomeWhenEnumerableIsNotEmpty()
+        {
+            var target = new[] { 1 };
+            var result = await Task.FromResult(target).SomeNotEmptyAsync(() => new object());
+            result.Should().HaveSome().Which.Should().BeSameAs(target);
+        }
+
+        [Fact]
+        public async Task SomeNotEmptyAsyncReturnsNoneWhenEnumerableIsEmpty()
+        {
+            var result = await Task.FromResult(Enumerable.Empty<int>()).SomeNotEmptyAsync();
+            result.Should().BeNone();
         }
 
         [Fact]
         public async Task SomeNotEmptyAsyncReturnsSomeWhenEnumerableIsNotEmpty()
         {
-            var enumerable = new[] { 1 }.AsEnumerable();
-            var result = await Task.FromResult(enumerable).SomeNotEmptyAsync(() => new object());
-            // ReSharper disable once PossibleMultipleEnumeration - enumerable is an array
-            result.Should().Be(enumerable.Some().WithException(new object()));
+            var target = new[] { 1 };
+            var result = await Task.FromResult(target).SomeNotEmptyAsync();
+            result.Should().HaveSome().Which.Should().BeSameAs(target);
+        }
+
+        [Fact]
+        public async Task SomeNotNullAsyncReturnsNoneWhenTaskIsNull()
+        {
+            var result = await Task.FromResult((string?) null).SomeNotNullAsync();
+            result.Should().BeNone();
+        }
+
+        [Fact]
+        public async Task SomeNotNullAsyncReturnsSomeWhenTaskIsNotNull()
+        {
+            var result = await Task.FromResult("test").SomeNotNullAsync();
+            result.Should().HaveSome().Which.Should().Be("test");
+        }
+
+        [Fact]
+        public async Task SomeNotNullAsyncWithExceptionReturnsNoneWhenTaskIsNull()
+        {
+            var exception = new object();
+            var result = await Task.FromResult<string?>(null).SomeNotNullAsync(() => exception);
+            result.Should().BeNone().Which.Should().BeSameAs(exception);
+        }
+
+        [Fact]
+        public async Task SomeNotNullAsyncWithExceptionReturnsSomeWhenTaskIsNotNull()
+        {
+            var result = await Task.FromResult("test").SomeNotNullAsync(() => default(object));
+            result.Should().HaveSome().Which.Should().Be("test");
         }
     }
 }
