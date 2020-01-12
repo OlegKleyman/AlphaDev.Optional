5f9661651e08c33f9f42a32dfb3b9033e48ccabc
Omego2K <omego2k@gmail.com>
2020-01-12 10:38:31 -0500

    add unsafe extensions

---
+++ core/AlphaDev.Optional.Extensions/Unsafe/OptionEitherExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Optional;
+
+namespace AlphaDev.Optional.Extensions.Unsafe
+{
+    public static class OptionEitherExtensions
+    {
+        public static TException ExceptionOrFailure<T, TException>(this Option<T, TException> option)
+        {
+            return option.Match(_ => throw new InvalidOperationException("Option has some."),
+                exception => exception);
+        }
+
+        public static TException ExceptionOrFailure<TValue, TException>(this Option<TValue, TException> option, Action fail)
+        {
+            return option.Match(value =>
+            {
+                fail();
+                throw new InvalidOperationException("Option has some.");
+            }, exception => exception);
+        }
+
+        public static TValue ValueOrFailure<TValue, TException>(this Option<TValue, TException> option, Action fail)
+        {
+            return option.ValueOr(() =>
+            {
+                fail();
+                throw new InvalidOperationException("Option is none.");
+            });
+        }
+    }
+}
---
+++ tests/unit/AlphaDev.Optional.Extensions.Tests.Unit/Unsafe/OptionEitherExtensionsTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using AlphaDev.Optional.Extensions.Unsafe;
+using FluentAssertions;
+using Optional;
+using Xunit;
+
+namespace AlphaDev.Optional.Extensions.Tests.Unit.Unsafe
+{
+    public static class OptionEitherExtensionsTests
+    {
+        [Fact]
+        public static void ExceptionOrFailureReturnsExceptionWhenOptionIsNone()
+        {
+            Option.None<string, string>("ex").ExceptionOrFailure().Should().Be("ex");
+        }
+
+        [Fact]
+        public static void ExceptionOrFailureThrowsInvalidOperationExceptionWhenOptionHasSome()
+        {
+            Action exceptionOrFailure = () => Option.Some<string?, string>(default).ExceptionOrFailure();
+            exceptionOrFailure.Should().Throw<InvalidOperationException>().WithMessage("Option has some.");
+        }
+
+        [Fact]
+        public static void ExceptionOrFailureWithActionExecutesFailureActionWhenOptionHasSome()
+        {
+            var executed = false;
+            try
+            {
+                Option.Some<string?, string>(default).ExceptionOrFailure(() => executed = true);
+            }
+            catch (InvalidOperationException)
+            {
+                executed.Should().BeTrue();
+            }
+        }
+
+        [Fact]
+        public static void ExceptionOrFailureWithActionReturnsExceptionWhenOptionIsNone()
+        {
+            Option.None<string, string>("ex").ExceptionOrFailure(() => { }).Should().Be("ex");
+        }
+
+        [Fact]
+        public static void ExceptionOrFailureWithActionThrowsInvalidOperationExceptionWhenOptionHasSome()
+        {
+            Action exceptionOrFailure = () => Option.Some<string?, string>(default).ExceptionOrFailure(() => { });
+            exceptionOrFailure.Should().Throw<InvalidOperationException>().WithMessage("Option has some.");
+        }
+
+        [Fact]
+        public static void ValueOrFailureWithActionExecutesFailureActionWhenOptionIsNone()
+        {
+            var executed = false;
+            try
+            {
+                Option.None<string, string?>(default).ValueOrFailure(() => executed = true);
+            }
+            catch (InvalidOperationException)
+            {
+                executed.Should().BeTrue();
+            }
+        }
+
+        [Fact]
+        public static void ValueOrFailureWithActionReturnsValueWhenOptionHasSome()
+        {
+            Option.Some<string, string>("value").ValueOrFailure(() => { }).Should().Be("value");
+        }
+
+        [Fact]
+        public static void ValueOrFailureWithActionThrowsInvalidOperationValueWhenOptionHasNone()
+        {
+            Action valueOrFailure = () => Option.None<string, string?>(default).ValueOrFailure(() => { });
+            valueOrFailure.Should().Throw<InvalidOperationException>().WithMessage("Option is none.");
+        }
+    }
+}
